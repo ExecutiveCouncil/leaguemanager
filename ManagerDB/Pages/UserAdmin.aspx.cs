35ab782d0e5b1b6ca2d7d9138f0cf684c704a536
aketzamp <aketzamp@hotmail.com>
2017-01-24 21:02:35 +0100

    UserAdmin
    Inicio UserAdmin

---
+++ ManagerDB/Pages/UserAdmin.aspx.cs
@@ -0,0 +1,136 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ManagerDB.Pages
+{
+    public partial class UserAdmin : BasicPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                var admin = false;
+                if (this.usuario != null)
+                {
+                    var _usuario = this.manager.t_users
+                            .Where(a => a.id == this.usuario.id).FirstOrDefault();
+                    if (_usuario != null)
+                    {
+                        if (_usuario.security_level == 1)
+                        {
+                            admin = true;
+                        }
+                    }
+                }
+
+                if (this.ValidateSession() == false || !admin)
+                {
+                    FormsAuthentication.RedirectToLoginPage();
+                }
+                else
+                {
+                    this.MostrarDatosUsuarios();
+                }
+            }
+        }
+
+        protected void MostrarDatosUsuarios()
+        {
+            var usuarios = this.manager.t_users.ToList();
+
+            if (usuarios.Count > 0)
+            {
+                this.GrUsuarios.DataSource = usuarios;
+                this._LbNoUsuarios.Visible = false;
+            }
+            else
+            {
+                this.GrUsuarios.DataSource = null;
+                this._LbNoUsuarios.Visible = true;
+            }
+            this.GrUsuarios.DataBind();
+        }
+
+        protected void GrUsuarios_ItemCommand(object source, DataGridCommandEventArgs e)
+        {
+            int _idUser = Convert.ToInt32(e.Item.Cells[0].Text);
+            switch (e.CommandName)
+            {
+                case "Editar":
+                {
+                    LimpiarPopUp();
+                    this.lblId.Text = _idUser.ToString();
+                    this.txtNSocio.Text = e.Item.Cells[1].Text;
+                    this.PopUpUser.Show();
+                    break;
+                }
+                case "Eliminar":
+                {
+                    var usuario = this.manager.t_users.Where(a=> a.id == _idUser).FirstOrDefault();
+                    this.manager.t_users.Remove(usuario);
+                    this.manager.SaveChanges();
+                    break;
+                }
+            }
+            this.GrUsuarios.DataBind();
+        }
+
+        protected void BtAddUser_Click(object sender, EventArgs e)
+        {
+            LimpiarPopUp();
+            this.PopUpUser.Show();
+        }
+
+        private void LimpiarPopUp()
+        {
+            this.lblId.Text = string.Empty;
+            this.txtNSocio.Text = string.Empty;
+        }
+
+        protected void btnGuardar_Command(object sender, CommandEventArgs e)
+        {
+            t_users nuevoUsuario= new t_users();
+            
+            if (this.lblId.Text == string.Empty)
+            {
+                //Calcular Id
+                nuevoUsuario.id = ObtenerIdUsuario();
+            }
+            else
+            {
+                var id = Convert.ToInt32(this.lblId.Text);
+                nuevoUsuario = this.manager.t_users.Where(a => a.id == id).FirstOrDefault();
+            }
+
+            nuevoUsuario.numero_socio = Convert.ToInt32(this.txtNSocio.Text);
+
+            if (this.lblId.Text == string.Empty)
+            {
+                this.manager.t_users.Add(nuevoUsuario);
+            }
+            this.manager.SaveChanges();
+        }
+
+        private int ObtenerIdUsuario()
+        {
+            var ultimoUsuario = (from m in this.manager.t_users
+                             select m).OrderByDescending(a => a.id).FirstOrDefault();
+            var idUsuario = 0;
+            if (ultimoUsuario == null)
+            {
+                idUsuario = 1;
+            }
+            else
+            {
+                idUsuario = ultimoUsuario.id + 1;
+            }
+            return idUsuario;
+        }
+    }
+}
---
+++ ManagerDB/Pages/UserAdmin.aspx.designer.cs
@@ -0,0 +1,141 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ManagerDB.Pages {
+    
+    
+    public partial class UserAdmin {
+        
+        /// <summary>
+        /// Control BtBack.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton BtBack;
+        
+        /// <summary>
+        /// Control _LbTitle.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label _LbTitle;
+        
+        /// <summary>
+        /// Control LbUsers.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LbUsers;
+        
+        /// <summary>
+        /// Control GrUsuarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataGrid GrUsuarios;
+        
+        /// <summary>
+        /// Control _LbNoUsuarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label _LbNoUsuarios;
+        
+        /// <summary>
+        /// Control PnlUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel PnlUsuario;
+        
+        /// <summary>
+        /// Control BtAddUser.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtAddUser;
+        
+        /// <summary>
+        /// Control btnShow.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnShow;
+        
+        /// <summary>
+        /// Control PopUpUser.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ModalPopupExtender PopUpUser;
+        
+        /// <summary>
+        /// Control PnlPopUp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel PnlPopUp;
+        
+        /// <summary>
+        /// Control btnClose.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnClose;
+        
+        /// <summary>
+        /// Control lblId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblId;
+        
+        /// <summary>
+        /// Control txtNSocio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNSocio;
+        
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+    }
+}
